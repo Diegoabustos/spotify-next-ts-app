4b77060a0d111934130be513ce93fd0a49f06c10
Diegoabustos <you@example.com>
2022-08-23 23:09:41 -0500

    feat: 🎸 Build functions to fetch data
    fetch artist id artist for get albums

--- config.ts
+++ config.ts
@@ -4,8 +4,11 @@ const TOKEN_URL: string = 'https://accounts.spotify.com/api/token';
 const CLIENT_ID : string | undefined = process.env.CLIENT_ID;
 const CLIENT_SECRET : string | undefined = process.env.CLIENT_SECRET;
 
+const API_TOKEN: string =  `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+
 export {
     TOKEN_URL,
     CLIENT_ID,
-    CLIENT_SECRET
+    CLIENT_SECRET,
+    API_TOKEN
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,61 @@
 import type { NextPage } from 'next'
+import { ChangeEvent, useEffect, useState } from 'react';
+import { TOKEN_URL, API_TOKEN } from '../config';
+
 
 const Home: NextPage = () => {
+  const [searchInput, setSearchInput] = useState('')
+  const [accesToken, setAccesToken] = useState('');
+  const [albums, setAlbums] = useState([])
+  const handleChange = ({target}: ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(target.value)
+  }
+  const handleSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
+    // 👇️ prevent page refresh
+    event.preventDefault();
+  };
+  useEffect(() => {
+    // API Access Token
+    let authParameters = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `${API_TOKEN}`
+    }
+    fetch(`${TOKEN_URL}`, authParameters)
+    .then(result => result.json())
+    .then(data => setAccesToken(data.access_token))
+  }, [])
+
+  // Search
+  async function search()  {
+
+    // Get request using search to get the Artist  ID
+    let searchParameters = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accesToken}`  
+      }
+    }
+    let artistID = await fetch(`https://api.spotify.com/v1/search?q=${searchInput}&type=artist`, searchParameters)
+      .then(response => response.json())
+      .then(data => { return data.artists.items[0].id })
+
+      // Get request with Artist ID grab all the albums from that artist
+      let returnAlbums = await fetch(`https://api.spotify.com/v1/artists/${artistID}/albums?include_groups=album&market=US&limi=50`, searchParameters)
+        .then(response => response.json())
+        .then(data => {
+          setAlbums(data.items)
+        })
+    }
   return (
     <div>
-      Next Project with typescript
+      <form onSubmit={handleSubmit}>
+        <input  onChange={handleChange} />
+        <button onClick={search}>search</button>
+      </form>
     </div>
   )
 }
