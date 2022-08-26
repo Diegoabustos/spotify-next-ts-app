5c1a88d11d3f86244ebd2d8bedc9d4e0cda0523d
Diegoabustos <you@example.com>
2022-08-25 23:58:01 -0500

    docs: ✏️ Add instructions to the project in readme file

--- hooks/useFetchToken.ts
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@tanstack/react-query"
-import { fetchToken } from "../api/fetchFunctions"
-
-
-export const useFetchToken = () => {
-    return useQuery(['token'], () => fetchToken())
-}
